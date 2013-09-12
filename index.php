89ef75a906c325eaabd85f9a55157029d04dd1db
Guilherme Henrique <guilherme.henrique@apiki.com>
2013-09-12 13:46:48 -0300

    Alteracoes diversas UI

--- index.php
+++ index.php
@@ -3,78 +3,89 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title>POMODORANO</title>    
-	<link rel="stylesheet" type="text/css" href="http://bootsnipp.com/css/bootstrap-v3.min.css?v=<?php echo filemtime('css/style.css'); ?>">
-	
 	<link rel="stylesheet" type="text/css" href="css/style.css?v=<?php echo filemtime('css/style.css'); ?>">
-	<script type='text/javascript' src='http://code.jquery.com/jquery-1.9.1.js'></script>
 </head>
 <body>
 	<div class="container">
-		<div class="row panel panel-default">
-			<div class="tabbable tabs-left">
-				<ul class="nav nav-tabs">
-					<li class="active"><a href="#tab1" data-toggle="tab" id="home"><i class="icon-home icon-5x"></i></a></li>
-					<li><a href="#tab2" data-toggle="tab" id="stats"><i class="icon-time icon-5x"></i></a></li>
-					<li><a href="#tab3" data-toggle="tab"><i class="icon-edit icon-5x"></i></a>
-					<li>
-						<a href="#r" data-toggle="tab" id="callyt"><i class="icon-plus icon-5x"></i></a><input type="text" id="yt">
-					</li>
-				</ul>
-				<div class="tab-content">
-					<div class="tab-pane active " id="tab1">
-						<blockquote>
-							<p>Crie um novo ciclo</p>
-						</blockquote>	
-						<div class="span8 panel panel-default inner">		
-							<div class="btn-toolbar">
-								<button class="btnn btn-flat add"><i class="icon-plus"></i> Novo pomodoro</button>
-								<button class="btn save" onclick="">Salvar</button>
-								<button class="btn export pull-right"><i class="icon-download-alt"></i> Exportar</button>					
-							</div>
-							<table id="pomodoros" class="table table-hover table-striped table-condensed">
-								<thead>
-									<tr>
-										<th style="width:15px">#</th>
-										<th style="width:120px">Projeto</th>
-										<th>Descrição</th>
-										<th style="width:85px">Duração</th>
-										<th style="width:20px">Estimativa</th>
-										<th style="width:20px">OK</th>
-									</tr>
-								</thead>
-								<tbody></tbody>
-							</table>
-						</div>
-					</div>	
-					<div class="tab-pane" id="tab2">
-						<blockquote>
-							<p>Sua tarefa atual</p>
-						</blockquote>
-						<div id="status" class="alert alert-info">
-							<div class="span4">
-								<div id="detail"></div>
-								<a class="start"></a>
-								<a class="finished"></a>
-							</div>
-							<div class="span4 pull-right" id="clock">
-								<h1 id='ht'></h1>
-								<span id="counter" class="well"></span>
-							</div>
-						</div>
-					</div>
-					<div class="tab-pane" id="tab3">
-						<blockquote>
-							<p>Anotações Miscelância</p>
-						</blockquote>
-						<div class="tabbable">
-							<ul class="nav nav-tabs" id="Tab">
-								<li class=""><a href="#notepad1" data-toggle="tab">Notepad</a></li>
-								<li><a href="javascript:;" id="addT"><i class="icon-plus"></i></a></li>
+		<div class="row">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+							<div class="logo"></div>
+					 JSPomodoro
+					</h3>
+				</div>
+				<div class="panel-body">
+					<div class="tabbable tabs-left">
+						<ul class="nav nav-tabs">
+							<li class="active"><a href="#tab1" data-toggle="tab" id="home"><i class="icon-home icon-3x"></i></a></li>
+							<li><a href="#tab2" data-toggle="tab" id="stats"><i class="icon-time icon-3x"></i></a></li>
+							<li><a href="#tab3" data-toggle="tab"><i class="icon-edit icon-3x"></i></a>
+								<li>
+									<a href="#r" data-toggle="tab" id="callyt"><i class="icon-plus icon-3x"></i></a><input type="text" id="yt">
+								</li>
 							</ul>
-							<div id="notepad" class="tab-content">
-								<div class="tab-pane active" id="notepad1">
-									<h4>Notepad #1</h4>
-									<textarea class="form-control txt" id="m"></textarea>
+							<div class="tab-content">
+								<div class="tab-pane active " id="tab1">
+									<div class="bs-callout bs-callout-info">
+										<h4>Crie um novo ciclo Pomodoro</h4>
+										<p>Geralmente isso varia de pessoa, porém o mais usado é 25 min para trabalhar e 5 descansando.</p>
+									</div>	
+									<div class="span8 panel panel-default inner">		
+										<div class="btn-toolbar">
+											<button class="btnn btn-flat add"><i class="icon-plus"></i> Novo pomodoro</button>
+											<button class="btn save" onclick="">Salvar</button>
+											<button class="btn export pull-right"><i class="icon-download-alt"></i> Exportar</button>					
+										</div>
+										<table id="pomodoros" class="table table-hover table-striped table-condensed">
+											<thead>
+												<tr>
+													<th style="width:15px">#</th>
+													<th style="width:120px">Projeto</th>
+													<th>Descrição</th>
+													<th style="width:85px">Duração</th>
+													<th style="width:20px">Estimativa</th>
+													<th style="width:20px">OK</th>
+												</tr>
+											</thead>
+											<tbody></tbody>
+										</table>
+									</div>
+								</div>	
+								<div class="tab-pane" id="tab2">
+									<div class="bs-callout bs-callout-info">
+										<h4>Sua tafera atual</h4>
+										<p>Aqui é descrito e cronometrado o serviço que você está a fazer neste momento.</p>
+									</div>	
+									<div id="status" class="alert alert-info">
+										<div class="span4">
+											<div id="detail"></div>
+											<a class="start"></a>
+											<a class="finished"></a>
+										</div>
+										<div class="span4 pull-right" id="clock">
+											<h1 id='ht'></h1>
+											<span id="counter" class="well"></span>
+										</div>
+									</div>
+								</div>
+								<div class="tab-pane" id="tab3">
+									<div class="bs-callout bs-callout-info">
+										<h4>Anotações miscelância</h4>
+										<p>Guarde suas notas rápidas aqui, crie quantos notepads forem necesários(máx 9).</p>
+									</div>	
+									<div class="tabbable">
+										<ul class="nav nav-tabs" id="Tab">
+											<li class=""><a href="#notepad1" data-toggle="tab">Notepad</a></li>
+											<li><a href="javascript:;" id="addT"><i class="icon-plus"></i></a></li>
+										</ul>
+										<div id="notepad" class="tab-content">
+											<div class="tab-pane active" id="notepad1">
+												<h4>Notepad #1</h4>
+												<textarea class="form-control txt" id="m"></textarea>
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -82,11 +93,13 @@
 				</div>
 			</div>
 		</div>
-	</div>
-
+	<script type='text/javascript' src="js/jquery.js?v=<?php echo filemtime('js/jquery.js'); ?>"></script>.
+	<script type='text/javascript' src="js/pomodoro.js?v=<?php echo filemtime('js/pomodoro.js'); ?>"></script>.
+	<script type='text/javascript' src="js/textarea.js"></script>
 	<script type='text/javascript' src="js/bootstrap.min.js"></script>
+	<script type='text/javascript' src="js/modal.js"></script>
 	<script type='text/javascript' src="http://www.csvscript.com/dev/html5csv.js"></script> 
-	<script type='text/javascript' src="js/pomodoro.min.js?v=<?php echo filemtime('js/pomodoro.min.js'); ?>"></script> 
+<!--	<script type='text/javascript' src="js/pomodoro.min.js?v=<?php echo filemtime('js/pomodoro.min.js'); ?>"></script> -->
 </body>
 </html>
 
--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,11 +1,14 @@
 jQuery(function (e) {
 
     e(function () {
+        //call init()
         POMODORO.init();
     });
 
     var POMODORO = {
         init: function () {
+            // initialize all as bootstrap
+
             this.startX();
             this.Table2CSV();
             this.PreventAccidentalClose();
@@ -14,21 +17,29 @@ jQuery(function (e) {
         },
         startX: function () {
             window.history.pushState("object or string", "Title", "#ready");
-
+            // once user mouveover the title has been reset
             e(document).on('mouseover', function () {
                 document.title = "POMODORANO";
             });
-            e('.finished').on('click', function () {
-                var plr = document.getElementById('d');
-                plr.play();
-            });
+
+            // e('.finished').on('click', function () {
+            //     var plr = document.getElementById('d');
+            //     plr.play();
+            // });
+            
+            //when page loads hide stats panel
             e('#stats').hide();
+
+            // when page loads hide the pomodoros table
             e('#pomodoros').hide();
+
             var _thisClock = null,
                 _thisStart = 0,
                 _thisTarget = e('#ht'),
                 _thisPaused = false,
                 index = 1;
+
+            // YouTube PoupUp
             e("#yt").keyup(function (e) {
                 if (e.which == 13) {
                     window.history.replaceState("object or string", "Title", "#VideoContext");
@@ -46,53 +57,85 @@ jQuery(function (e) {
                     })
                 }
             });
+            /*
+            * Initializete the prompt for setuo your pomodoro
+            */
             e('.add').on('click', function () {
+                // set it pased true while user choosing the time
                 _thisPaused = true;
+                // prompt where user will enter the "Project Title"
                 var typer = prompt('Nome do Projeto');
+
+                // once user was inputed a project title...
                 if (typer !== null) {
                     var desc = prompt('Descrição breve do projeto/Tarefa'),
                         est = prompt('Estimativa(em Minutos)');
                 } else {
                     return;
                 }
+
                 index++;
+
                 if (typer === null && desc == null) {
+                    // set it paused  false if vars typer & desc was no index
                     _thisPaused = false;
+                    //return false
                     return;
                 } else {
                     _thisPaused = false;
+                    // select the target div where infos will appear
                     eelm = e('#detail');
 
+                    // variables to get content of eelem
                     var InnerStatusWork = eelm.find('#currentWork').text(),
                         Status = InnerStatusWork.split(':'),
                         InnerStatusTask = e('#TaskPanel').text(),
                         StatusTask = InnerStatusTask.split(':'),
                         Timex = e('#ht').text();
-
+                    // splits variables into a new index
                     ProjTitle = Status[1];
                     ProjDesc = StatusTask[1];
 
-
+                    // check again if project title was set... if true
                     if (ProjTitle !== undefined) {
+
+                        // display table pomodoros with your details
                         e('#pomodoros').fadeIn();
+
+                        // reset our clock
                         _thisTarget.html('00:00:00,000');
+
+                        // clear the intervals reset
                         clearInterval(_thisClock);
+
+                        // set clock as null
                         _thisClock = null;
 
+                        // pad number check if is a single number
                         var esTx = (est.length > 1) ? "00:" + est : '00:0' + est;
 
+                        // add in the pomodoros table your curreently time of the work, with expect and much more
                         e('#pomodoros > tbody:last').append('<tr><td>' + index + '</td><td>' + ProjTitle + '</td><td>' + ProjDesc + '</td><td>' + Timex + '</td><td>' + esTx + '</td><td><i class="icon-check-sign icon-large"></i></td></tr>');
+                        
+                        // THIS STILL IN TESTS
                         e('.save').on('click', function () {
                             e('#pomodoros > tbody:last').append('<tr><td>' + index + '</td><td>' + ProjTitle + '</td><td>' + ProjDesc + '</td><td>' + Timex + '</td><td><i class="icon-check-sign icon-large"></i></td>' + ZeroBefore(est) + 'Min</tr>');
                         });
                     }
+                    // clear element id have any data form previously pomodoro
                     eelm.empty().append('<div id="currentWork"><h1>Trabalhando em:</h1><h2>' + typer + '</h2></div><div id="TaskPanel"><span>Tarefa:</span><br><p id="task" class="well well-small">' + desc + '</p></div>');
                 }
-
+                // simulate virual click our tigger for start timer
                 e('.start').click();
 
             });
             var i = 1;
+            /*
+            *
+            * This will allow user to add mutiples notepads. It's based ina index variable what has incremented every time
+            * the element with the ID #addT has clicked.
+            *
+            */
             e('#addT').on('click', function () {
 
                 i++;
@@ -105,6 +148,9 @@ jQuery(function (e) {
             });
 
 
+            /*
+            * If YouTube or "+" was been clickec will show input for user insert data.
+            */
             e('#callyt').on('click', function () {
                 e("#yt").fadeIn(function () {
                     e(this).animate({
@@ -126,10 +172,20 @@ jQuery(function (e) {
                 CSV.begin('#pomodoros').download('Export.csv').go();
             });
 
-
+             /**
+             * this function will display stopwatch(not the "timer")
+             * @constructor
+             * @param {inval} d - a timestamp formated day/hour/secound
+             */
             function RenderStopWatch(d) {
                 return ZeroBefore(d.getUTCHours()) + ':' + ZeroBefore(d.getMinutes()) + ':' + ZeroBefore(d.getSeconds()) + ',' + ZeroBefore(d.getMilliseconds());
             }
+             /**
+             * Add the 0 before number if its a single number (i.e. 12:3 will be replaced by 12:03, note the 0 intestead)
+             * @constructor
+             * @param {inval} n - a number, single or not
+             * @param {inval} lenght - dynamic set
+             */
 
             function ZeroBefore(n, length) {
                 if (typeof (length) == 'undefined') {
@@ -140,19 +196,23 @@ jQuery(function (e) {
                 }
                 return n;
             }
-
+            /**
+             * Do the timer tick increment every secound a variable.
+             * @constructor
+             * @param {inval} minutes - a intval of minutes will be processed by loop
+             */
             function countdown(minutes) {
                 var seconds = 60;
                 var mins = minutes
 
-                    function tick() {
+                    function loop() {
 
                         var counter = document.getElementById("counterms");
-                        var current_minutes = mins - 1
+                        var _that = mins - 1
                         seconds--;
-                        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+                        counter.innerHTML = _that.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
                         if (seconds > 0) {
-                            setTimeout(tick, 1000);
+                            setTimeout(loop, 1000);
                         } else {
                             if (mins > 1) {
                                 countdown(mins - 1);
@@ -160,38 +220,59 @@ jQuery(function (e) {
                         }
                     }
 
-                tick();
+                loop();
             }
 
             var o = 0;
-
+            // setting the tigger will start all operation
             e('.start').on('click', function () {
                 o++;
                 //var durac = 0;
+
+                // disable button to add more pomodoros
                 e(".add").prop('disabled', true);
+                // set pushstate for url and making a back event to prevent conflits when 2 user uses it at same time
                 window.history.replaceState("object or string", "Title", "#" + Math.random().toString(36).substr(2));
+
+                // expects fro pomodoro
                 var durac = prompt('Duração do Pomodoro(em minutos, Ex 25 para 25Min)');
+
+                // if user set a valid expect for pomodoro the tab with ID #stats appears 
                 if (durac !== "") {
                     e('#stats').fadeIn(2000);
                 } else {
                     e('#stats').hide();
                 }
 
+                // select element for display my countdown
                 var countdown = e("#counter");
 
-
+                // multiply the minutes by secounds
                 var totalTime = durac * 60;
 
+                /*
+                * This function prevent to show single number, just remember this function is for countdown,
+                * because this i needed to create another.
+                *
+                * @param {intval} n - a int val date timestamp formated
+                */
                 function pad(n) {
                     return n > 9 ? "" + n : "0" + n;
                 }
                 var original = totalTime;
 
+                /*
+                * This function prevent to show single number, just remember this function is for countdown,
+                * because this i needed to create another.
+                * This is just for minutes.
+                *
+                * @param {intval} n - a int val date timestamp formated
+                */
                 function _minFormat(n) {
                     return original >= 600 && n <= 9 ? "0" + n : "" + n;
                 }
 
-
+                // set a counter and every time it repeat it decrease a number
                 var interval = setInterval(function () {
 
                     clockLoop();
@@ -202,9 +283,10 @@ jQuery(function (e) {
                     }
                 }, 1000);
 
-
+                /*
+                * This function render the negative clock container
+                */
                 function render() {
-
                     var m = Math.floor(totalTime / 60);
                     var seconds = totalTime % 60;
 
@@ -214,6 +296,9 @@ jQuery(function (e) {
                     countdown.html(m + ":" + seconds);
                 }
 
+                /*
+                *this function is for easy stop/reset and start the function
+                */
                 function clockLoop() {
                     render();
                     totalTime--;
@@ -222,9 +307,11 @@ jQuery(function (e) {
                 clockLoop();
 
 
-                if (!_thisClock) {
-
-                    _thisStart = new Date();
+                if (!_thisClock) { // if clock is false
+                    //now
+                    _thisStart = new Date()
+                    
+                    // the clock
                     _thisClock = setInterval(function () {
                             if (_thisPaused === true) {
                                 return;
